23f8584423d1b16bb3fbc688cd9cbb36bdea5735
David <david.crittin@students.hevs.ch>
2020-02-29 12:15:14 +0100

    Fix car plus de menu en mode desktop

--- ressources/js/script.js
+++ ressources/js/script.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', function(){
-    var elems = document.querySelectorAll('body > section > nav > ul > li:not([class="hamburger"])');
-    var index = 0, length = elems.length;
-    for ( ; index < length; index++) {
-        elems[index].style.visibility = "hidden";
+    if(window.getComputedStyle(document.querySelector('li.hamburger')).display !== "none") {
+        var elems = document.querySelectorAll('body > section > nav > ul > li:not([class="hamburger"])');
+        var index = 0, length = elems.length;
+        for ( ; index < length; index++) {
+            elems[index].style.visibility = "hidden";
+        }
+        document.querySelector('body > section > nav > ul').style.height = "50px";
     }
-    document.querySelector('body > section > nav > ul').style.height = "50px";
 }, false);
 
 function displayMenu() {
