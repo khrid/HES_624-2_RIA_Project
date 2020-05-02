75b30e725349c00704bd5db5b205017aebc7a2c9
albaindufils <albain.dufils@crem.ch>
2020-05-02 13:29:19 +0200

    Test export

--- game/js/constants.js
+++ game/js/constants.js
@@ -2,4 +2,7 @@ export const LEVEL_ONE = 1;
 export const LEVEL_TWO = 2;
 export const LEVEL_THREE = 3;
 export const LEVEL_FOUR = 4;
-export const DEPART_PLAYER = 75;
+const DEPART_PLAYER = 75;
+
+
+export default {DEPART_PLAYER}
