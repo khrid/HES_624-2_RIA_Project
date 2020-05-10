588ddeb875f2fd572de0c88ba8e120d5bc8f8e3e
Louise <bretzlouise@gmail.com>
2020-05-10 15:16:45 +0200

    ajout des levels

--- game/js/localstorage.js
+++ game/js/localstorage.js
@@ -2,14 +2,12 @@ function setUser(username){
     window.localStorage.setItem('name', username); 
 }
 
-
 function getImagePath(){
     name= window.localStorage.getItem('name');
     imageWin = document.getElementById('imgwin');
-    imageWin.src = "images/bitmoji/"+name+"_win.png";
+    imageWin.setAttribute("src", "images/bitmoji/"+name+"_win.png");
+
 }
-
-
 
 console.log("localstorage.js included");
 
